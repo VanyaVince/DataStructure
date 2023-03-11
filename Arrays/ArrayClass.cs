a08e2e8a93a809e5cbfeb1de89ba9018e92b8e4c
Ivan Svirydovich <ivan.svirydovich@intapp.com>
2023-03-11 23:12:40 +0100

    add gitignore file

---
+++ Arrays/ArrayClass.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace LinearDataStructure.Arrays
+{
+    public class ArrayClass
+    {
+        private int[] array;
+        private int count;
+
+        public ArrayClass(int size)
+        {
+            array = new int[size];
+        }
+
+        public void Insert(int value)
+        {
+            if (array.Length == count)
+            {
+                var arrayCopy = new int[count * 2];
+
+                for (int i = 0; i < array.Length; i++)
+                    arrayCopy[i] = array[i];
+
+                array = arrayCopy;
+            }
+
+            array[count++] = value;
+        }
+
+        public void RemoveAt(int indexToRemove)
+        {
+            if (indexToRemove > array.Length)
+                throw new ArgumentOutOfRangeException();
+
+            for (int i = indexToRemove; i < count; i++)
+                array[i] = array[i + 1];
+           
+            count--;
+
+            //var arrayToCopy = new int[array.Length - 1];
+            //var indexOffset = 0;
+
+            //for (int i = 0; i < arrayToCopy.Length; i++)
+            //{
+            //    if (i == indexToRemove)
+            //    {
+            //        indexOffset = 1;
+            //    }
+
+            //    arrayToCopy[i] = array[i + indexOffset];
+            //}
+
+            //array = arrayToCopy;
+        }
+
+        public int IndexOf(int number)
+        {
+            int indexToLookUp = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    indexToLookUp = i;
+                }
+            }
+
+            return indexToLookUp;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < count; i++)
+                Console.WriteLine(array[i]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LinearDataStructure.Arrays;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var numbers = new ArrayClass(3);
+
+        numbers.Insert(10);
+        numbers.Insert(20);
+        numbers.Insert(30);
+        numbers.Insert(40);
+        numbers.RemoveAt(0);
+        numbers.Print();
+    }
+}
